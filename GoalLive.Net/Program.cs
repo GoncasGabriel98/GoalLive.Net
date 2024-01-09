a6ad92eae19c74b932234f2655fc2cdd984f582f
GoncasGabriel98 <goncas.gabriel@gmail.com>
2024-01-09 18:13:00 +0000

    Check API Status

--- GoalLive.Net/Program.cs
+++ GoalLive.Net/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiUrl = "https://api.sofascore.com/api/v1/sport/football/scheduled-events/"; // SofaScore API
+
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("API is OK. Status Code: " + response.StatusCode);
+                }
+                else
+                {
+                    Console.WriteLine("API is not OK. Status Code: " + response.StatusCode);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
